0679eb03e778c56dcb0a3790b1b70bdf21dfa4e3
diegovillegas <diego.villegas@menloschool.org>
2024-08-28 00:26:25 -0700

    Added comments

--- src/Riddler.java
+++ src/Riddler.java
@@ -15,6 +15,7 @@ public class Riddler {
         int i = 0;
         int ascii = 0;
         while(i < encrypted.length()){
+            // if chart at i is anything but a letter
             if((int) encrypted.charAt(i) < 65 || (int) encrypted.charAt(i) > 122){
                 decrypted += encrypted.charAt(i);
             }
@@ -25,6 +26,7 @@ public class Riddler {
                 if(Character.isUpperCase(encrypted.charAt(i))){
                     // check if ascii value is too big
                     if(ascii > 90){
+                        // makes sure ascii value is a upper case letter
                         ascii = (ascii - 90) + 65;
                     }
                 }
@@ -32,9 +34,11 @@ public class Riddler {
                 else{
                     // check if ascii value is too big
                     if(ascii > 122){
+                        // makes sure ascii value is a lower case letter
                         ascii = (ascii - 122) + 97;
                     }
                 }
+                // Convert int to string
                 decrypted += String.valueOf(ascii);
             }
             i++;
@@ -51,10 +55,13 @@ public class Riddler {
         int i = 0;
         // TODO: Complete the decryptTwo() function.
         while(i < encrypted.length()){
+            // If char is a space
             if(encrypted.charAt(i) == ' '){
                 i++;
+                // Turn num into a string
                 letter = String.valueOf(Integer.parseInt(num));
                 decrypted += letter;
+                // Reset
                 letter = "";
                 num = "";
             }
@@ -72,6 +79,7 @@ public class Riddler {
         while(i < encrypted.length()){
             // Grab each byte
             binary = encrypted.substring(i, i + 7);
+            // Convert String(byte) to String
             decrypted += Integer.parseInt(binary, 2);
             i = i + 8;
         }
